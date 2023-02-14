c02be01d82101c8ca46c5b3d7c336273b34b7cdd
Panagiotis Rigas <prigas96@gmail.com>
2023-02-14 22:35:42 +0200

    added metrics to utilities

---
+++ utils/metrics.py
@@ -0,0 +1,71 @@
+# save here metric for tests
+import numpy as np
+
+# define Linf() function for a square and a point
+def Linf(square, point):
+    """
+        Compute the Linf metric for a square and a point
+        
+        Parameters:
+            square (np.array): square coordinates
+            point (np.array): point coordinates
+            
+        Returns:
+            min_dist (float): minimum distance between the point and the square
+    """
+    # 1st put coords around point and transform
+    # in order to do the transform substract the point from each coord
+    square_new = np.array([np.subtract(square_point, point) for square_point in square])
+    # find if y = abs(x) intersects the square and if so add the point to the list so that the clockwise order is preserved
+    # find the intersection point
+    # find if y = abs(x) intersects the square
+    # for each edge of the square, check if y = abs(x) intersects the edge
+    for i in range(square.shape[0]):
+        sq_pt1 = square_new[i]
+        sq_pt2 = square_new[(i+1)%square.shape[0]]
+        # alpha 
+        if not (sq_pt2[0] - sq_pt1[0] == 0):
+            a = (sq_pt2[1] - sq_pt1[1])/(sq_pt2[0] - sq_pt1[0])
+        else:
+            continue
+        # beta
+        b = sq_pt1[1] - a*sq_pt1[0]
+        # new point
+        if not (a == 1 or a == -1):
+            new_pts = np.array([[b/(a-1), b/(a-1)],[b/(a+1), -b/(a+1)]])
+        else:
+            continue
+        # check if new points lie inside the segment
+        # if so add to square_new
+        for new_pt in new_pts:
+            if (new_pt[0] >= min(sq_pt1[0], sq_pt2[0])) and (new_pt[0] <= max(sq_pt1[0], sq_pt2[0])) and \
+                (new_pt[1] >= min(sq_pt1[1], sq_pt2[1])) and (new_pt[1] <= max(sq_pt1[1], sq_pt2[1])):
+                square_new = np.insert(square_new, i+1, new_pt, axis=0)
+    # get Linf from each point in square_new
+    min_dist = np.inf
+    for square_point in square_new:
+        dist = np.max(np.abs(square_point))
+        if dist < min_dist:
+            min_dist = dist
+    return min_dist
+
+    
+# define simplified Linf() function for a square and a point
+def Linf_(square, q_point):
+    """
+        Compute the simplified Linf metric for a square and a point
+    
+        Parameters:
+            square (np.array): square coordinates  
+            q_point (np.array): point coordinates
+        
+        Returns:
+            min_dist (float): minimum distance between the point and the square
+
+    """
+    min_dist = np.inf
+    for point in square:
+        dist = np.max(np.abs(np.subtract(point, q_point)))
+        if dist < min_dist:
+            min_dist = dist
+    return min_dist
