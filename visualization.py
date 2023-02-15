f232e27c3da8238f4071bf6561c2bc74899acb15
Minas Dioletis <92537784+minasd1@users.noreply.github.com>
2023-02-15 10:07:08 +0200

    Slight modifications made to run properly

--- visualization.py
+++ visualization.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-import geometry as geo
+from utils import geometry as geo
 from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 
