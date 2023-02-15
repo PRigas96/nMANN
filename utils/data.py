31e16fcabe802a6a348f692a240a9c6a8189c422
Panagiotis Rigas <prigas96@gmail.com>
2023-02-15 16:28:04 +0200

    added querry points generator in data.py

--- utils/data.py
+++ utils/data.py
@@ -38,3 +38,30 @@ def rotations(mu, sigma, num):
                     value = np.random.normal(mu[j], sigma[j])
             r_list.append(value)
     return r_list
+
+# generate querry points
+def gen_qpoints(squares, num_pts):
+    """
+        Generate querry points that are not inside the squares
+
+        Parameters:
+            squares (np.array): list of squares, each square is a 1x4 array of center of mass, length, and rotation
+            num_pts (int): number of points to generate
+            
+        Returns:
+            list: list of points that are not inside the squares
+    """
+    q_pts = []
+    while len(q_pts) < num_pts:
+        if len(q_pts) % 1000 == 0:
+            print("Number of points generated: ", len(q_pts))
+        # generate a point from x = [3,297] and y = [3,297]
+        x = np.random.randint(3, 297)
+        y = np.random.randint(3, 297)
+        # check if the point is inside the square
+        label = False
+        for sq_pt in squares:
+            if geo.IsPointInsidePoly([x,y], geo.create_square2(sq_pt)):
+                label = True
+        if not label:
+            q_pts.append([x,y])
